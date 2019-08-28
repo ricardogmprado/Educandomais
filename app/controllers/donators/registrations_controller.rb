19c90df2ab0bcd11cf626c39775566f36cba1688
Ricardo Prado <ricardogmprado@gmail.com>
2019-08-28 15:25:49 +0800

    modified devise forms for each user type. Its all good now

--- app/controllers/donators/registrations_controller.rb
+++ app/controllers/donators/registrations_controller.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
 class Donators::RegistrationsController < Devise::RegistrationsController
-  before_action :configure_donator_permitted_parameters [:create, :update]
+  # before_action :configure_sign_up_params, only: [:create]
+  # before_action :configure_account_update_params, only: [:update]
+  # prepend_before_action :verify_signed_out_user, only: :destroy
 
- def configure_donator_permitted_parameters
+ def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys:[:name, :last_name, :email, :password, :password_confirmation, :cpf, :telephone])
     devise_parameter_sanitizer.permit(:account_update, keys:[:name, :last_name, :email, :password, :password_confirmation, :cpf, :telephone])
   end
