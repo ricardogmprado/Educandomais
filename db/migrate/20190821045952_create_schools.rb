2c0de8fc2ae31ca3dd38efefec82dced7df0bbdc
Ricardo Prado <ricardogmprado@gmail.com>
2019-09-12 20:17:26 +0800

    trying to make heroku work

--- db/migrate/20190821045952_create_schools.rb
+++ db/migrate/20190821045952_create_schools.rb
@@ -5,7 +5,6 @@ class CreateSchools < ActiveRecord::Migration[5.2]
       t.float :longitude
       t.string :address
       t.string :name
-      t.string :neighbourhood
       t.string :city
       t.string :state
       t.string :zipcode
--- db/migrate/20190822010245_remove_neighbourhood_from_schools.rb
+++
@@ -1,5 +0,0 @@
-class RemoveNeighbourhoodFromSchools < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :schools, :neighbourhood, :string
-  end
-end
