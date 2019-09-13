3aeeaa3692fccec5ab140d2fa7fe115e08873b1a
Ricardo Prado <ricardogmprado@gmail.com>
2019-09-13 20:42:39 +0800

    working on project views

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -1,10 +1,10 @@
-class SchoolsController < ApplicationController
-  # GET /schools
-  # GET /schools.json
-  def index
-  url = 'http://www.educacao.sp.gov.br/central-de-atendimento/consulta.asp?'
-  unparsed_page = open(url).read
-  parsed_page = Nokogiri::HTML(unparsed_page)
+# class SchoolsController < ApplicationController
+#   # GET /schools
+#   # GET /schools.json
+#   def index
+#   url = 'http://www.educacao.sp.gov.br/central-de-atendimento/consulta.asp?'
+#   unparsed_page = open(url).read
+#   parsed_page = Nokogiri::HTML(unparsed_page)
 
-  end
+#   end
 
