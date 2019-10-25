1d6f4dcfe700adfb35087f52eac23ead1567c6b5
Ricardo Prado <ricardogmprado@gmail.com>
2019-10-25 11:50:39 +0800

    now project status change according to donations ammount

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -4,4 +4,14 @@ class Donation < ApplicationRecord
   validates :quantity_donated, presence: true
   validates :status, presence: true
 
+  after_update :check_status
+
+  def check_status
+    if self.status == "doação recebida"
+      project = self.project
+      project.total_donations += self.quantity_donated
+      project.save
+    end
+  end
+
 end
